b5c42ce37d68d04e05bf10ca3e13a9a8d87e1a12
Vinod KR <109779300+vin-dKR@users.noreply.github.com>
2024-01-27 18:48:30 +0530

    full responsive pendchooo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,10 +8,6 @@ function Navbar() {
       <div className='navbar'>
 
           <img src="https://i.ibb.co/X2rsKwZ/Plash-Logo-2.png" alt="jjj" />
-
-          // <div className="search">
-          //     <input className='input__box' type="text" />
-          // </div>
           <div className="post-btn">
             <a href="/contact">Contact</a>
           </div>

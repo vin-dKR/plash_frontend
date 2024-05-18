5b0927de7847d6567aaf8cdbdeb4bf580c5d940e
vin-dKR <vinodkumarmurmu62@gmail.com>
2024-05-18 17:51:07 +0530

    now h

---
+++ src/components/fuck.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function fuck() {
+  return (
+    <div>
+     i know that is a toatal dick man he was a very nice work in hjis padev\\\
+     <div className="container">
+        <h1 className="heading">
+            
+            hii every one this is me Vinod KR Murmu and i am a good boy. i know there are many whjo get theri own bussness
+        </h1>
+        </div> 
+    </div>
+  )
+}
+
+export default fuck
